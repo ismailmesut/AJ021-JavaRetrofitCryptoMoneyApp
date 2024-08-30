0d3093a35d8f3b412ddaf4f4a436dcd6d5d9a541
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-30 19:23:04 +0300

    updated: adding retrofit and rxjava libraries in build.gradle(app)

--- app/src/main/java/com/ismailmesutmujde/javaretrofitcryptomoneyapp/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/javaretrofitcryptomoneyapp/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //https://api.nomics.com/v1/prices?key=2187154b76945f2373394aa34f7dc98a
+        //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
+
     }
 }
