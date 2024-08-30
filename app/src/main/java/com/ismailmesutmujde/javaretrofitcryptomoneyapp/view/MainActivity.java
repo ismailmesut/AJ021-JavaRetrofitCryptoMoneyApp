750df691492e4fa156ef7ccd25437d83bf39a3bc
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-30 20:25:42 +0300

    updated: get cryptodatas

--- app/src/main/java/com/ismailmesutmujde/javaretrofitcryptomoneyapp/view/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/javaretrofitcryptomoneyapp/view/MainActivity.java
@@ -3,10 +3,26 @@ package com.ismailmesutmujde.javaretrofitcryptomoneyapp.view;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.ismailmesutmujde.javaretrofitcryptomoneyapp.R;
+import com.ismailmesutmujde.javaretrofitcryptomoneyapp.model.CryptoModel;
+import com.ismailmesutmujde.javaretrofitcryptomoneyapp.service.CryptoAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<CryptoModel> cryptoModels;
+    private String BASE_URL = "https://raw.githubusercontent.com";
+    Retrofit retrofit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +31,38 @@ public class MainActivity extends AppCompatActivity {
         //https://api.nomics.com/v1/prices?key=2187154b76945f2373394aa34f7dc98a
         //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
 
+        // Retrofit & JSON
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        loadData();
+    }
+
+    private void loadData() {
+        CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
+        Call<List<CryptoModel>> call = cryptoAPI.getData();
+        call.enqueue(new Callback<List<CryptoModel>>() {
+            @Override
+            public void onResponse(Call<List<CryptoModel>> call, Response<List<CryptoModel>> response) {
+                if(response.isSuccessful()) {
+                    List<CryptoModel> responseList = response.body();
+                    cryptoModels = new ArrayList<>(responseList);
+
+                    for (CryptoModel cryptoModel : cryptoModels) {
+                        System.out.println(cryptoModel.currency);
+                        System.out.println(cryptoModel.price);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<CryptoModel>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 }
